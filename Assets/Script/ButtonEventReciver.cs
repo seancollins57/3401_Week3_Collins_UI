7130eb2fe036487df1286970db7ea9ac3ef5d027
Sean Collins <sc679115@ohio.edu>
2019-02-05 14:01:29 -0500

    Initial commit

---
+++ Assets/Script/ButtonEventReciver.cs
@@ -0,0 +1,31 @@
+﻿//these are libraries that include code options
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEventReciver : MonoBehaviour
+{
+   
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+
+    }
+
+    //a custom function that'll make the buttom do stuff
+    public void OnButtonPress ()
+
+    {
+        //for organization
+        Debug.Log("Button Pressed");
+    }
+}
---
+++ Assets/Script/NotAButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotAButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPress()
+
+    {
+        Debug.Log("Title Pressed"); }
+
+        public class TitleClicked : EventTrigger;
+
+
+    }
+
+}
